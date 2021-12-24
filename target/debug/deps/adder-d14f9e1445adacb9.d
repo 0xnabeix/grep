83a75bcc6b9e7bd7769d194fef221936d774d6b3
torutona <sololouve@gmail.com>
2021-12-24 12:50:47 -0800

    cmd search tool

---
+++ target/debug/deps/adder-d14f9e1445adacb9.d
@@ -0,0 +1,5 @@
+/home/ruster/Documents/adder/target/debug/deps/adder-d14f9e1445adacb9: src/lib.rs
+
+/home/ruster/Documents/adder/target/debug/deps/adder-d14f9e1445adacb9.d: src/lib.rs
+
+src/lib.rs:
---
+++ target/debug/deps/integration_test-9734655cde68cce7.d
@@ -0,0 +1,6 @@
+/home/ruster/Documents/adder/target/debug/deps/integration_test-9734655cde68cce7: tests/integration_test.rs tests/common/mod.rs
+
+/home/ruster/Documents/adder/target/debug/deps/integration_test-9734655cde68cce7.d: tests/integration_test.rs tests/common/mod.rs
+
+tests/integration_test.rs:
+tests/common/mod.rs:
